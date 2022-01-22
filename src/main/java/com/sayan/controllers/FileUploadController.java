6cd2350cdb5a85e1df696d279dbeaa7de0056d2a
Cadris <cadrissilver@gmail.com>
2022-01-23 01:39:56 +0530

    Working Some What

--- src/main/java/com/sayan/controllers/FileUploadController.java
+++ src/main/java/com/sayan/controllers/FileUploadController.java
@@ -1,5 +1,14 @@
 package com.sayan.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FileUploadController {
 
+	@PostMapping("/upload-file")
+	public ResponseEntity<String> uploadFile(){
+		return ResponseEntity.ok("Working");
+	}
 }
